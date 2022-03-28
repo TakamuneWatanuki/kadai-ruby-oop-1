57fcb52e678a0aa45d1a8ebb1de65c7573ab7096
takamune.watanuki <tenshinw@outlook.com>
2022-03-28 12:46:13 +0000

    drawの引数を追加

--- team.rb
+++ team.rb
@@ -2,11 +2,11 @@ class Team
   
   attr_accessor :name, :win, :lose, :draw
   
-  def initialize(name, win, lose)
+  def initialize(name, win, lose, draw)
     self.name = name
     self.win = win
     self.lose = lose
-    self.draw = 120 - (win + lose)
+    self.draw = draw
   end
   
   def calc_win_rate
@@ -14,16 +14,16 @@ class Team
   end
   
   def show_team_result
-    puts "#{self.name} の2022年の成績は #{self.win}勝 #{self.lose}敗 #{self.draw}分、勝率は #{self.calc_win_rate}です。"
+    puts "#{self.name} の2020年の成績は #{self.win}勝 #{self.lose}敗 #{self.draw}分、勝率は #{self.calc_win_rate}です。"
   end
 end
 
-giants = Team.new("Giants", 67, 45)
-tigers = Team.new("Tigers", 60, 53)
-dragons = Team.new("Dragons", 60, 55)
-bayStars = Team.new("BayStars", 56, 58)
-carp = Team.new("Carp", 52, 56)
-swallows = Team.new("Swallows", 41, 69)
+giants = Team.new("Giants", 67, 45, 8)
+tigers = Team.new("Tigers", 60, 53, 7)
+dragons = Team.new("Dragons", 60, 55, 5)
+bayStars = Team.new("BayStars", 56, 58, 6)
+carp = Team.new("Carp", 52, 56, 12)
+swallows = Team.new("Swallows", 41, 69, 10)
 
 giants.show_team_result
 tigers.show_team_result
